d15761649d818863abe5c9f3bd9f540dc26f8ef7
Leonardo Bighetti <leonardo.bighetti@locaweb.com.br>
2012-08-10 20:54:39 -0300

    scaffolding

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,29 @@
 set :application, "darkrails"
 set :repository,  "git@github.com:darkside/darkrails.git"
 
-set :scm, :subversion
+
+set :user, "cproruby2"
+
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "186.202.122.168"                          # Your HTTP server, Apache/etc
 role :app, "186.202.122.168"                          # This may be the same as your `Web` server
 role :db,  "186.202.122.168", :primary => true        # This is where Rails migrations will run
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+  # SCM settings
+set :scm, :git
+set :branch, 'master'
+set :deploy_to, "/home/cproruby2"
+set :deploy_via, :remote_cache
+set :keep_releases, 3
+set :git_enable_submodules, true
+set :rails_env, 'production' unless exists?(:rails_env)
+set :use_sudo, false
+
+# Git settings for capistrano
+default_run_options[:pty]   = true
+ssh_options[:forward_agent] = true
+
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
