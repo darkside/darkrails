9fc0918df518e25a66380597253957648e2dab8d
Leonardo Bighetti <leonardo.bighetti@locaweb.com.br>
2012-08-10 20:34:39 -0300

    Vai curintia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darkrails::Application.config.secret_token = '3878dfb1559661409f1a2a9d213ca7be09a77271024b45ccfbbd787b73d73c08c3d389cd267667cf2db986cd818bc5a8a9ca1c34359aa796bc60685765ab15f1'
